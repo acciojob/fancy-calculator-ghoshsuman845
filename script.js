155780a28ef59e9c8671ba83df7f4c04e82bf66e
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-11 08:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let num1;
+
+			// Get the input for the first variable using the prompt() function
+			num1 = prompt("Enter the first number:");
+
+			// Convert the input to a number
+			num1 = parseFloat(num1);
+
+			// Multiply the first number by 2
+			let result = num1 * 2;
+
+			// Declare the second variable
+			let num2;
+
+			// Get the input for the second variable using the prompt() function
+			num2 = prompt("Enter the second number:");
+
+			// Convert the input to a number
+			num2 = parseFloat(num2);
+
+			// Add the second number to the result
+			result += num2;
+
+			alert("The final result is: " + result);
